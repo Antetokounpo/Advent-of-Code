779b9463822fcc9bb3a39a19a237ce3d43490d98
Antetokounpo <antor.232@outlook.com>
2020-12-20 00:15:02 -0500

    Update lib

--- lib/AOC.hs
+++ lib/AOC.hs
@@ -16,18 +16,6 @@ splitOn' c str cstr = if isPrefixOf c w then (cstr ++ m) : (splitOn' c (drop (le
 splitOn :: Eq a => [a] -> [a] -> [[a]]
 splitOn c str = splitOn' c str []
 
-{-
-splitOn :: Eq a => [a] -> [a] -> [[a]]
-splitOn c [] = []
-splitOn c str = if isPrefixOf c w then m : (splitOn c (drop (length c) w)) else (k : k') : rstr'
-                   where f = head c
-                         w = dropWhile (/= f) str
-                         m = takeWhile (/= f) str
-                         (k:rstr) = str
-                         (k':rstr') = splitOn c rstr
--}
-
-
 readInput :: IO String
 readInput = do
         (filename:_) <- getArgs
