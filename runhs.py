4a487c21d9ddcd1fac413605f4f833deacdb8f38
Antetokounpo <antor.232@outlook.com>
2021-07-03 14:21:50 -0400

    Update runhs script

--- runhs.py
+++ runhs.py
@@ -7,5 +7,9 @@ y, d, p = sys.argv[1:]
 
 path = "{y}/day{d}".format(y=y, d=d)
 
-os.system("runhaskell -ilib {path}/part{p} {path}/input".format(path=path, p=p))
+r = os.system("ghc -O2 -ilib {path}/part{p}".format(path=path, p=p))
+if r != 0:
+    sys.exit(1)
+print("Running part{p}.hs...".format(p=p))
+os.system("{path}/part{p} {path}/input".format(path=path, p=p))
 
